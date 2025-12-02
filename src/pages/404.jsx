cd0f59f89d1fe7186dd593e9c6a76be2bb4b2f58
EGOUCHOLA <egoucholamalick229@gmail.com>
2025-12-02 15:04:54 +0100

    404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,18 @@
+import "../css/404.css";
+import FooterLink from "../components/FooterLink";
 
 
 export default function NotFound() {
-
-    return (
-        <section>
-            
-        </section>
-    )
+  return (
+    <div className="error-page">
+      <div className="error-content">
+        <h1>404</h1>
+        <p>Vous vous êtes perdu dans vos notes...</p>
+        <div className="error-logo">
+          <img src="../../public/assets/logo404.png" alt="" />
+        </div>
+      </div>
+      <FooterLink /> 
+    </div>
+  );
 }
